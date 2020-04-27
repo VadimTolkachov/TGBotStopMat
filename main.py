b8ef254965960150ae94a15996880b19acf1b58d
VadimTolkachov <46502188+VadimTolkachov@users.noreply.github.com>
2020-04-28 00:10:59 +0300

    Add main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import const
 import re
 import telebot
-cheklist = ('епта','сучара','хуйня','блять')
 
+cheklist = ('епта', 'сучара', 'хуйня', 'блять')
 bot = telebot.TeleBot(const.token)
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-
     text_message = message.text.lower()
-    text_message = re.sub(r'[?|$|.|!|,]',r'',text_message)
+    text_message = re.sub(r'[?|$|.|!|,]', r'', text_message)
     text_message = text_message.split(' ')
     for word in text_message:
-
         if word in cheklist:
             bot.delete_message(message.chat.id, message.message_id)
             break
 
+
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
